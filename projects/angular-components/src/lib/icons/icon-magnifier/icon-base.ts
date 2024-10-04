5f89535f5b3720a2eddbad37d173214e2e2d2e34
Vivy <4380412+Matthbo@users.noreply.github.com>
2024-10-04 15:09:10 +0200

    Improve styling of all components based on figma dev mode

--- projects/angular-components/src/lib/icons/icon-magnifier/icon-base.ts
+++ projects/angular-components/src/lib/icons/icon-magnifier/icon-base.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, numberAttribute } from '@angular/core';
 
 @Component({
   template: '',
 })
 export class IconBaseComponent {
-  @Input() width: number = 24;
-  @Input() height: number = 24;
+  @Input({ transform: numberAttribute }) width: number = 24;
+  @Input({ transform: numberAttribute }) height: number = 24;
   @Input() colour: string = '#000';
 }
